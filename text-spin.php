8514c1b667a36b2f5182a1e6e75fc62cdb489f05
wayan wahyu adi putra <33746001+yanwahyu@users.noreply.github.com>
2019-10-05 22:34:58 +0800

    menambahkan fungsi spin
    menambahkan fungsi spin untuk menampilkan text menjadi satu huruf

--- text-spin.php
+++ text-spin.php
@@ -22,3 +22,27 @@ foreach ($replaceWith as $key) {
 
 }
 print_r($new_str);
+// menghasilkan satu text
+function spin($text) {
+   //While there's pairs of square brackets, spin whats inside of them
+   while (!(strpos($text,'{') === FALSE) && !(strpos($text,'}') === FALSE)) {
+       //Find the first '['
+       $leftb = strpos($text,'{');
+
+       //Find the first ']'
+       $rightb = strpos($text,'}');
+
+       //Split the string up, then (psudo)randomise the item chosen
+       $spintext = explode('|',substr($text,$leftb+1,$rightb-$leftb-1));
+       $spinselect = trim($spintext[mt_rand(0,count($spintext)-1)]);
+
+       //Get the whole string to replace including the brackets
+       $brackettext = substr($text,$leftb,($rightb-$leftb)+1);
+
+       //Replace the [blah,blah2] with whatever item is chosen
+       $text = str_replace($brackettext,$spinselect,$text);
+   }
+   //return the block of modified text
+   return $text;
+}
+echo spin($new_str);
